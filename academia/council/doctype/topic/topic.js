8fb45ed5b22a7bdfff31185e52c7b2add41ece16
Muhannad-Al-Mozaiger <programmer22muhannad@gmail.com>
2024-01-16 01:56:57 +0300

    fix(Council): Update doctype names and adding naming series to doctypes

--- academia/council/doctype/topic/topic.js
+++ academia/council/doctype/topic/topic.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2024, SanU and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Council Session", {
+// frappe.ui.form.on("Topic", {
 // 	refresh(frm) {
 
 // 	},
--- academia/council/doctype/topic/topic.py
+++ academia/council/doctype/topic/topic.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class CouncilTopic(Document):
+class Topic(Document):
 	pass
