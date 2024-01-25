f76aace14856210678cff40334415059bfc9df08
Al-Mozaiger <mygameali653@gmail.com>
2024-01-26 02:14:09 +0300

    feat(Councils): Add member duplicate check functionality to Council doctype and updating fields
    This commit adds functionality to the Council doctype that checks for duplicate members. When a faculty member is selected in the Council Member doctype the system now validates if the member already exists in other rows. If a duplicate is found, the faculty_member field is cleared, and an message is displayed indicating the duplication along with the row number.
    
    The added code includes the setup function in the Council doctype, which defines the check_member_duplicate function responsible for performing the duplicate check. Additionally, the faculty_member event in the Council Member doctype triggers the duplicate check when the faculty_member field is modified.
    
    and adds a check for duplicate members in the validate function of the Council document class. The check_member_duplicate function has been implemented to extract member names from the members field and iterate through them to identify duplicates. If duplicates are found, an error is thrown using the frappe.throw function with a translated error message.
    
    The code has been tested and verified to ensure that duplicate members cannot be added to the Council document.

--- academia/councils/doctype/council/council.js
+++ academia/councils/doctype/council/council.js
@@ -1,8 +1,36 @@
 // Copyright (c) 2024, SanU and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Council", {
-// 	refresh(frm) {
+frappe.ui.form.on("Council", {
+    // Setup function called when the Council form is loaded
+    setup: function (frm) {
+        // Function to check for duplicate members
+        frm.check_member_duplicate = function (frm, row) {
+            // Iterate through each member in the form's members field
+            frm.doc.members.forEach(member => {
+                // Check if the current row's faculty_member is not empty or the same as the current member being iterated
+                if (!(row.faculty_member == '' || row.idx == member.idx)) {
+                    // Check if the current row's faculty_member is the same as the member's faculty_member
+                    if (row.faculty_member == member.faculty_member) {
+                        // Clear the faculty_member value in the current row
+                        row.faculty_member = '';
+                        // Refresh the members field in the form to reflect the changes
+                        frm.refresh_field('members');
+                        // message indicating that the member name already exists in a specific row
+                        msgprint(__(`${member.member_name} already exists in row ${member.idx}`));
+                    }
+                }
+            });
+        }
+    },
+});
 
-// 	},
-// });
+frappe.ui.form.on('Council Member', {
+    // Event triggered when the faculty_member field changes in the Council Member form
+    faculty_member: function (frm, cdt, cdn) {
+        // Get the current row
+        let row = locals[cdt][cdn];
+        // Call the check_member_duplicate function to check for duplicate members
+        frm.check_member_duplicate(frm, row);
+    }
+});
