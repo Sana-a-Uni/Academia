67ef8453e11363cf5776316961a1ef3f7f03bef8
Muhannad-Al-Mozaiger <programmer22muhannad@gmail.com>
2024-01-26 02:14:09 +0300

    feat(Councils): Add member duplicate check functionality to Council doctype and updating fields
    This commit adds functionality to the Council doctype that checks for duplicate members. When a faculty member is selected in the Council Member doctype the system now validates if the member already exists in other rows. If a duplicate is found, the faculty_member field is cleared, and an message is displayed indicating the duplication along with the row number.
    
    The added code includes the setup function in the Council doctype, which defines the check_member_duplicate function responsible for performing the duplicate check. Additionally, the faculty_member event in the Council Member doctype triggers the duplicate check when the faculty_member field is modified.
    
    and adds a check for duplicate members in the validate function of the Council document class. The check_member_duplicate function has been implemented to extract member names from the members field and iterate through them to identify duplicates. If duplicates are found, an error is thrown using the frappe.throw function with a translated error message.
    
    The code has been tested and verified to ensure that duplicate members cannot be added to the Council document.

--- academia/councils/doctype/council/council.py
+++ academia/councils/doctype/council/council.py
@@ -1,9 +1,25 @@
 # Copyright (c) 2024, SanU and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class Council(Document):
-	pass
+    # Function to check for member duplicates
+    def check_member_duplicate(self):
+        # Extract member names from the members field
+        members = [member.member_name for member in self.members]
+        
+        # Iterate through each member
+        for member in members:
+            # Check if the member count is greater than 1, indicating a duplicate
+            if members.count(member) > 1:
+                return True
+    
+    def validate(self):
+        # Check for member duplicates
+        if self.check_member_duplicate():
+            # Throw an error if duplicates are found
+            frappe.throw(_(f"Council members can't be duplicated"))
