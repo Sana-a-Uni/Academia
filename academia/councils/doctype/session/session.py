08cc1fb67225a3d72f11e18f30881b80c581d0ad
Muhannad-Al-Mozaiger <programmer22muhannad@gmail.com>
2024-01-27 16:58:31 +0300

    feat(Councils): Implement fetch members functionality for linked council in Session doctype and updating fields
    This commit introduces a new feature to the Council doctype, enabling the retrieval of members from a linked council document. The code includes modifications to the Council doctype and its associated fields. The new functionality allows for fetching and displaying members from the linked council. The fields are updated accordingly to reflect the retrieved members. This enhancement improves the user experience by automating the retrieval and display of council members based on the linked document.

--- academia/councils/doctype/session/session.py
+++ academia/councils/doctype/session/session.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2024, SanU and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class Session(Document):
-	pass
+		@frappe.whitelist()
+		def get_linked_council(self,docname):
+			# Check if the 'Council' document with the given docname exists in the database
+			if frappe.db.exists('Council', docname):
+				# Retrieve and return the 'Council' document as a Frappe document object
+				return frappe.get_doc('Council', docname)
