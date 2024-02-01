22afe6f6d70ffd5501840958ab852b94aae9bce5
ju-omari21 <jullunaromari@gmail.com>
2024-02-01 06:04:56 +0300

    feat: Adding Validation to the First, Middle, Last Name

--- academia/academia/doctype/faculty_member/faculty_member.py
+++ academia/academia/doctype/faculty_member/faculty_member.py
@@ -1,12 +1,23 @@
 # Copyright (c) 2023, SanU and contributors
 # For license information, please see license.txt
 
-# import frappe
 from frappe.model.document import Document
-
+import frappe
+import re
 
 class FacultyMember(Document):
-	def autoname(self):
-		self.full_name = self.first_name + " " + self.middle_name + " " + self.last_name
+    def autoname(self):
+        self.full_name = self.first_name + " " + self.middle_name + " " + self.last_name
+
+    def validate(self):
+
+        if not re.match("^[a-zA-Z]*$", self.first_name):
+            frappe.throw("First name should only contain letters")
+
+        if not re.match("^[a-zA-Z]*$", self.middle_name):
+            frappe.throw("Middle name should only contain letters")
+
+        if not re.match("^[a-zA-Z]*$", self.last_name):
+            frappe.throw("Last name should only contain letters")
 
 
