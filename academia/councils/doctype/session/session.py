6dcfe919af7e8974382565d914a9c932478499e9
Muhannad-Al-Mozaiger <programmer22muhannad@gmail.com>
2024-02-03 00:09:21 +0300

    feat(Councils): Automate workflow for assignments upon session submission.
    - Added create_new_assignment function:
    - Simplifies creation of new Topic Assignment records.
    - Sets default status to Accepted for clarity.
    - Enhanced on_submit function:
    - Seamlessly handles postponed topics during submission.
    - Recreates assignments for continuity.
    - Updates original assignments with decision details for tracking.

--- academia/councils/doctype/session/session.py
+++ academia/councils/doctype/session/session.py
@@ -54,3 +54,57 @@ class Session(Document):
 		"""
 		topic_assignments = frappe.get_all("Topic Assignment",fields=["*"],filters={'docstatus': 0,"council":council,"status":"Accepted"})
 		return topic_assignments
+
+	def create_new_assignment(self,title: str,description:str,date,council,topic):
+		"""Creates a new Topic Assignment record with the specified details.
+
+		Args:
+			title (str): The title of the assignment.
+			description (str): The description of the assignment.
+			date (date): The assignment date.
+			council (str): The name of the council associated with the assignment.
+			topic (str): The name of the topic associated with the assignment.
+
+		Returns:
+			Document: The newly created Topic Assignment document.
+		"""
+		doc_assignment = frappe.new_doc("Topic Assignment")
+		doc_assignment.title=title
+		doc_assignment.description=description
+		doc_assignment.assignment_date=date
+		doc_assignment.council=council
+		doc_assignment.topic=topic
+		doc_assignment.status="Accepted"
+		doc_assignment.insert()
+		return doc_assignment
+   
+	# This function handles actions to be performed when the document is submitted.
+	def on_submit(self):
+		# Retrieve session assignments from the document
+		session_assignments = self.assignments
+
+		# Process each session assignment
+		for session_assignment in session_assignments:
+			# Retrieve details of the corresponding Topic Assignment
+			session_assignment_doc = frappe.get_value("Topic Assignment", session_assignment.topic_assignment, ["*"], as_dict=1)
+
+			# If the decision type is "Postponed":
+			if session_assignment.decision_type == "Postponed":
+				# Create a new assignment for postponed topics
+				if session_assignment_doc:
+					doc_assignment = self.create_new_assignment(
+						title=session_assignment_doc.title,
+						description=session_assignment_doc.description,
+						date=frappe.utils.nowdate(),
+						council=self.council,
+						topic=session_assignment_doc.topic
+					)
+					session_assignment.forward_assignment = doc_assignment.name  # Link the new assignment
+
+			# Update the Topic Assignment with the decision details
+			session_assignment_doc.decision = session_assignment.decision
+			frappe.db.set_value('Topic Assignment', session_assignment_doc.name, {
+				'decision': session_assignment.decision,
+				'docstatus': 1,  # Mark as submitted
+				'decision_type': session_assignment.decision_type
+			})
