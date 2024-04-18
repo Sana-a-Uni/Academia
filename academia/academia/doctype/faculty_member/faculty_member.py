2dcf96f42a4cd88a773cd168cd0e14127165c29d
ju-omari21 <jullunaromari@gmail.com>
2024-04-18 18:34:04 +0300

    refact: Modifing the Facluty Member Doctype

--- academia/academia/doctype/faculty_member/faculty_member.py
+++ academia/academia/doctype/faculty_member/faculty_member.py
@@ -6,18 +6,4 @@ import frappe
 import re
 
 class FacultyMember(Document):
-    def autoname(self):
-        self.full_name = self.first_name + " " + self.middle_name + " " + self.last_name
-
-    def validate(self):
-
-        if not re.match("^[a-zA-Z ]*$", self.first_name):
-            frappe.throw("First name should only contain letters")
-
-        if not re.match("^[a-zA-Z ]*$", self.middle_name):
-            frappe.throw("Middle name should only contain letters")
-
-        if not re.match("^[a-zA-Z ]*$", self.last_name):
-            frappe.throw("Last name should only contain letters")
-
-
+    pass
