32a10bcd52a498359be61cf9d7def07468b8ea33
eng-AmaniMo <eng.amani99@hotmail.com>
2024-04-22 22:29:54 +0300

    feat: Adding Conference and Workshop, University and Community Service, Award and Appreciation Certificate, Authority DocTypes to Academia module

--- academia/academia/doctype/faculty_member/faculty_member.py
+++ academia/academia/doctype/faculty_member/faculty_member.py
@@ -6,9 +6,4 @@ import frappe
 import re
 
 class FacultyMember(Document):
-    # def validate(self):
-    #     self.set_image_read_only()
-
-    # def set_image_read_only(self):
-    #     frappe.db.sql("UPDATE `tabDocField` SET `read_only` = 1 WHERE `parent` = %s AND `field_name` = 'image'", self.doctype)
     pass
