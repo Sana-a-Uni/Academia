4c22a718e73f0d572bf8282d7a163d566488c9b2
eng-AmaniMo <eng.amani99@hotmail.com>
2024-04-26 14:59:52 +0300

    feat: Adding validation for all URL fields and Attachment fields, in addition to Page Numbers of Academic Publication DocType

--- academia/academia/doctype/academic_publication/academic_publication.py
+++ academia/academia/doctype/academic_publication/academic_publication.py
@@ -1,9 +1,56 @@
 # Copyright (c) 2024, SanU and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+import re
 
 
 class AcademicPublication(Document):
-	pass
+    def validate(self):
+        # Calling functions
+        self.validate_page_numbers()
+        self.validate_url()
+
+
+    # FN: Verifying 'from_page', 'to_page' and 'page_numbers' fields
+    def validate_page_numbers(self):
+        # Verifying that both fields are filled, not just one
+        if (not self.from_page and self.to_page) or (self.from_page and not self.to_page):
+            frappe.throw("Please fill both 'From Page' and 'To Page' fields.")
+        # Verifying that both fields have non-null values
+        elif self.from_page and self.to_page:
+            # Verifying that both fields contain only numbers and this - character
+            if not self.from_page.replace('-', '').isdigit() or not self.to_page.replace('-', '').isdigit():
+                frappe.throw("Page numbers must be integer.")
+            # Verifying that both fields do not contain negative values 
+            elif int(self.from_page) < 0 or int(self.to_page) < 0:
+                frappe.throw("Page numbers must be positive.")
+            # Verifying that the first page is not greater than the last page
+            elif int(self.from_page) > int(self.to_page):
+                frappe.throw("First page must be less than last page.")
+            # Writing the correct values in a specific format
+            else:
+                if int(self.from_page) == int(self.to_page):
+                    self.page_numbers = self.from_page
+                else:
+                    self.page_numbers = f"{self.from_page} - {self.to_page}"
+        # Verifying that both fields have null values
+        else:
+            self.page_numbers = '0'
+            self.from_page = '0'
+            self.to_page = '0'
+    # End of the function
+
+
+    # FN: Verifying 'publication_link' field
+    def validate_url(self):
+        # Define a regular expression pattern for validating URLs
+        url_pattern = re.compile(r'^(http|https|ftp)://\S+$')
+        # Verifying that publication_link matches the URL pattern
+        if self.publication_link:
+            if not url_pattern.match(self.publication_link):
+                frappe.throw("Publication Link in not valid. Please enter a valid URL starting with http, https, or ftp")
+    # End of the function
+
+
--- academia/academia/doctype/scientific_article/scientific_article.py
+++ academia/academia/doctype/scientific_article/scientific_article.py
@@ -3,14 +3,6 @@
 
 from frappe.model.document import Document
 import frappe
-import re
 
 class ScientificArticle(Document):
-    def validate(self):
-        
-        if not re.match("^[a-zA-Z ]*$", str(self.supervisor_name)):
-            frappe.throw("Supervisor name should only contain letters and spaces")
-        
-        if not re.match("^[a-zA-Z ]*$", str(self.publisher_name)):
-            frappe.throw("Publisher name should only contain letters and spaces")
-
+    pass
