e16364f2b763e0af4b8ac71798831adf56204495
ju-omari21 <jullunaromari@gmail.com>
2024-04-28 19:59:37 +0300

    feat: Adding Validation to journal_name, hindex, issn fields

--- academia/academia/doctype/journal/journal.py
+++ academia/academia/doctype/journal/journal.py
@@ -11,6 +11,22 @@ class Journal(Document):
         # Calling functions
         self.validate_url()
 
+        if not re.match("^[A-Za-z\s]+$", self.journal_name):
+            frappe.throw("Journal name should only contain letters")
+
+        if self.hindex and not re.match("^[0-9]+$", str(self.hindex)):
+            frappe.throw("H-Index should only contain numbers")
+
+        if self.issn:
+            issn_str = str(self.issn).strip()  # Remove leading/trailing whitespaces
+            if not issn_str or not re.match(r"^\d{4}[-\s]?\d{4}$", issn_str):
+                # Only allow digits and hyphen
+                frappe.throw("ISSN should be in the format XXXX-XXXX or XXXX XXXX, where X represents a digit")
+
+            else:
+                # Format the ISSN as XXXX-XXXX
+                formatted_issn = re.sub(r"(\d{4})(\d{4})", r"\1-\2", issn_str)
+                self.issn = formatted_issn
 
     # FN: Verifying 'website_link' field
     def validate_url(self):
@@ -20,4 +36,8 @@ class Journal(Document):
         if self.website_link:
             if not url_pattern.match(self.website_link):
                 frappe.throw("Website Link is not valid. Please enter a valid URL starting with http, https, or ftp")
+<<<<<<< Updated upstream
     # End of the function
+=======
+    # End of the function
+>>>>>>> Stashed changes
