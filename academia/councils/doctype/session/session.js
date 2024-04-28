d1c0a6c3e4ecade20979746f7867c41a59a8304d
Muhannad-Al-Mozaiger <programmer22muhannad@gmail.com>
2024-04-29 02:01:22 +0300

    refactor(Session): refactor form scripts for improved clarity and efficiency.
    This commit refactors the JavaScript form scripts for the Session DocType to improve readability and efficiency. Changes include:
    
    - Consolidation of member and assignment fetch functions into more concise and modern JavaScript promises.
    - Removal of redundant API calls by utilizing frappe.db.get_doc and frappe.db.get_list for direct database queries.
    - Streamlined child table manipulations for members and assignments to reduce redundancy and enhance performance.
    
    These changes enhance the maintainability of the code and reduce the complexity of the operations involved in managing session details within the Frappe framework.

--- academia/councils/doctype/session/session.js
+++ academia/councils/doctype/session/session.js
@@ -1,63 +1,60 @@
 // Copyright (c) 2024, SanU and contributors
 // For license information, please see license.txt
+
 frappe.ui.form.on("Session", {
-    setup: function (frm) {
-        // method to fetch members from a council document
-        frm.fetch_members_from_council = function (council_docname) {
-            // call get_linked_council method from python api to get linked council
-            frm.call('get_linked_council', { docname: council_docname })
-                .then(r => {
-                    // Check if the method returned a value
-                    if (r.message) {
-                        // set linked council that returned 
-                        let linked_doc = r.message;
-                        // Clear existing members
-                        frm.doc.members = '';
-                        council_members = linked_doc.members;
-                        council_members.forEach(council_member => {
-                            // Add each council member to session members
-                            frm.add_child('members', {
-                                faculty_member: council_member.faculty_member,
-                                attendance: "Attend",
-                                member_role: council_member.member_role
-                            });
-                            // Refresh the 'members' field
-                            frm.refresh_field('members');
-                        });
-                    }
+    /**
+     * Fetches the members of the selected council and adds them to the session form.
+     * @param {object} frm - The current form object.
+     */
+    fetch_members_from_council(frm) {
+        // Retrieve council form for selected council
+        frappe.db.get_doc('Council', frm.doc.council).then(council => {
+            if (council.members) {
+                // Clear any existing members in the child table:
+                frm.doc.members = '';
+                // Loop through the council members and add them to the child table:
+                council.members.forEach(council_member => {
+                    frm.add_child('members', {
+                        faculty_member: council_member.faculty_member,
+                        attendance: 'Attend',
+                        member_role: council_member.member_role
+                    });
                 });
-        }
+                // Refresh the child table to display the new member:
+                frm.refresh_field('members');
+            }
+        });
     },
     council(frm) {
         // Event handler for 'council' field change
         if (frm.doc.council) {
             // Fetch members from the selected council
-            frm.fetch_members_from_council(frm.doc.council);
+            frm.trigger('fetch_members_from_council');
         }
     }
     ,
-    // Fetches assignments from the server and populates the 'assignments' child table.
+    /**
+     * Fetches the assignments for the current session.
+     * @param {Object} frm - The current form object.
+     */
     fetch_assignments(frm) {
-        // Call the server-side 'get_assignments' method to retrieve assignments:
-        frm.call('get_assignments', { council: frm.doc.council })
-            .then(r => {
-                // If assignments are received successfully:
-                if (r.message) {
-                    const assignments = r.message;
-
-                    // Clear any existing assignments in the child table:
-                    frm.doc.assignments = '';
-
-                    // Create child table rows for each assignment:
-                    assignments.forEach(assignment => {
-                        frm.add_child('assignments', {
-                            topic_assignment: assignment.name,
-                        });
-
-                        // Refresh the child table to display the new rows:
-                        frm.refresh_field('assignments');
-                    });
-                }
+        frappe.db.get_list("Topic Assignment", {
+            filters: {
+                docstatus: 0,
+                council: cur_frm.doc.council,
+                status: "Accepted"
+            }
+        }).then((assignments) => {
+            // Clear any existing assignments in the child table:
+            frm.doc.assignments = '';
+            // Loop through the assignments and add them to the child table:
+            assignments.forEach(assignment => {
+                frm.add_child('assignments', {
+                    topic_assignment: assignment.name,
+                });
             });
+            // Refresh the child table to display assignments:
+            frm.refresh_field('assignments');
+        });
     }
 });
