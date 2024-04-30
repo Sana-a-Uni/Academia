664627cf658a92a4512b178110da9af33ee04ad0
Ali Alnosairi <alialnosiari@gmail.com>
2024-04-30 19:22:54 +0300

    feat(councils): Enhance Topic Assignment with validation, dynamic queries, and UI updates
    - Implement server-side validation for main and sub-category relationships in Topic Assignment.
    - Enhance client script for Topic Assignment: Introduce custom button styling, add dynamic topic data fetching, and improve field interactions based on council and main category selections.
    - Develop server-side API function to selectively retrieve available topics based on council, enhancing data relevancy.
    - Update list view settings to display dynamic status indicators, improving visual feedback on record status.
    
    This update aims to streamline user interactions, enforce data integrity, and enhance the overall user experience in managing Topic Assignments.

--- academia/councils/doctype/topic_assignment/topic_assignment.py
+++ academia/councils/doctype/topic_assignment/topic_assignment.py
@@ -1,7 +1,9 @@
 # Copyright (c) 2024, SanU and contributors
 # For license information, please see license.txt
 
-# import frappe
+import json
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
@@ -22,11 +24,53 @@ class TopicAssignment(Document):
 		decision: DF.TextEditor | None
 		decision_type: DF.Literal["", "Postponed", "Resolved", "Transferred"]
 		description: DF.TextEditor
-		main_type: DF.Data | None
+		main_category: DF.Link | None
 		naming_series: DF.Literal["CNCL-TA-.{topic}.##"]
 		status: DF.Literal["", "Pending Review", "Pending Acceptance", "Accepted", "Rejected"]
-		subtype: DF.Data | None
+		sub_category: DF.Link | None
 		title: DF.Data
-		topic: DF.Link
+		topic: DF.Link | None
 	# end: auto-generated types
-	pass
+	def validate(self):
+		self.validate_main_sub_category_relationship()
+  
+	def validate_main_sub_category_relationship(self):
+		"""
+		Validate the relationship between main category and sub-category, and check that the selected sub category is one of the  selected main category's sub categories
+		"""
+		if(self.main_category and self.sub_category):
+			main_category_of_selected_sub=frappe.get_value("Topic Sub Category",self.sub_category,"main_category")
+			# Check if the main category of the sub-category does not  match the selected main category
+			if main_category_of_selected_sub!=self.main_category:
+				frappe.throw(
+					_("{0} is not sub category of {1}").format(self.sub_category,self.main_category)
+				)
+		
+		
+# Assuming your app structure supports this placement
+
+
+
+
+@frappe.whitelist()
+def get_available_topics(doctype, txt, searchfield, start, page_len, filters):
+    # Ensure that 'council' is provided in filters
+    # if not filters or 'council' not in filters:
+    #     frappe.throw(_("Filters must contain 'council'."))
+
+    return frappe.db.sql(
+        """SELECT name FROM `tabTopic`
+        WHERE council = %(council)s AND
+        name NOT IN (
+            SELECT topic
+            FROM `tabTopic Assignment`
+            WHERE council = %(council)s
+        )
+        ORDER BY name
+       """,
+        {
+            "council": filters.get("council")
+        },
+        as_list=True
+    )
+
