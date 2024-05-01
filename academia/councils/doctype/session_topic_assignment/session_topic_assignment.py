53608c69d4620d41f43b7826b43e06beadf49bed
Muhannad-Al-Mozaiger <programmer22muhannad@gmail.com>
2024-05-02 02:48:04 +0300

    fix(Councils): add properties for virtual fields in Session Topic Assignment.

--- academia/councils/doctype/session_topic_assignment/session_topic_assignment.py
+++ academia/councils/doctype/session_topic_assignment/session_topic_assignment.py
@@ -1,27 +1,35 @@
 # Copyright (c) 2024, SanU and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class SessionTopicAssignment(Document):
-	# begin: auto-generated types
-	# This code is auto-generated. Do not modify anything in this block.
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
 
-	from typing import TYPE_CHECKING
+    from typing import TYPE_CHECKING
 
-	if TYPE_CHECKING:
-		from frappe.types import DF
+    if TYPE_CHECKING:
+        from frappe.types import DF
 
-		decision: DF.TextEditor | None
-		decision_type: DF.Literal["", "Postponed", "Resolved", "Transferred"]
-		description: DF.TextEditor | None
-		forward_assignment: DF.Link | None
-		parent: DF.Data
-		parentfield: DF.Data
-		parenttype: DF.Data
-		title: DF.Data | None
-		topic_assignment: DF.Link
-	# end: auto-generated types
-	pass
+        decision: DF.TextEditor | None
+        decision_type: DF.Literal["", "Postponed", "Resolved", "Transferred"]
+        description: DF.TextEditor | None
+        forward_assignment: DF.Link | None
+        parent: DF.Data
+        parentfield: DF.Data
+        parenttype: DF.Data
+        title: DF.Data | None
+        topic_assignment: DF.Link
+    # end: auto-generated types
+    pass
+
+    @property
+    def description(self):
+        return frappe.get_value('Topic Assignment', self.topic_assignment, 'description')
+
+    @property
+    def title(self):
+        return frappe.get_value('Topic Assignment', self.topic_assignment, 'title')
