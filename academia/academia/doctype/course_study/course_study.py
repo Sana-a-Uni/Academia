fd0422be0863cd069d587d09d6d25f10adb7e043
tariqalsrory <tariqalsrory@gmail.com>
2024-05-07 16:59:45 +0300

    refact(core): refactoring course study and course Enrollment  doctypes

--- academia/academia/doctype/course_study/course_study.py
+++ academia/academia/doctype/course_study/course_study.py
@@ -6,4 +6,21 @@ from frappe.model.document import Document
 
 
 class CourseStudy(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		academic_term: DF.Link
+		academic_year: DF.Link
+		course_code: DF.Link
+		course_name: DF.Data | None
+		course_type: DF.ReadOnly | None
+		instructor: DF.Link | None
+		program: DF.Link
+		student_batch: DF.Link
+	# end: auto-generated types
 	pass
