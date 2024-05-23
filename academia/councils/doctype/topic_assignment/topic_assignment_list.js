cfbb7adc38fb5dc9949b43895aa0f1e619182154
Hamdi Mawari <almawryhmdyjmal@gmail.com>
2024-05-23 23:18:23 +0300

    feat(Councils): Add validation for 'Add to Group' action in Topic Assignment list:
    - Ensure selected topic assignments are not groups themselves.
    - Prevent adding topic assignments that are already part of another group.
    - Open the new Topic Assignment form only if all selected assignments pass validation.

--- academia/councils/doctype/topic_assignment/topic_assignment_list.js
+++ academia/councils/doctype/topic_assignment/topic_assignment_list.js
@@ -1,6 +1,6 @@
 frappe.listview_settings["Topic Assignment"] = {
 
-  onload: function(topic_assignment) {
+  onload: function (topic_assignment) {
 
     topic_assignment.page.add_action_item(__('Add to Group'), async function () {
       const selected_assignments = topic_assignment.get_checked_items(true);
@@ -12,31 +12,52 @@ frappe.listview_settings["Topic Assignment"] = {
       await frappe.model.with_doctype('Topic Assignment');
       var doc = frappe.model.get_new_doc('Topic Assignment');
       doc.is_group = 1;
+      let errors = [];
 
       for (let assignment of selected_assignments) {
         try {
           let { message: obj } = await frappe.db.get_value(
             "Topic Assignment",
             assignment,
-            ["title", "assignment_date"]
+            ["title",
+              "assignment_date",
+              "is_group",
+              "parent_assignment"
+            ]
           );
           if (obj) {
-            let new_child = frappe.model.add_child(
-              doc,
-              'Topic Assignment Copy',
-              'grouped_assignments'
-            );
-            new_child.topic_assignment = assignment;
-            new_child.title = obj.title;
-            new_child.assignment_date = obj.assignment_date;
+            if (obj.is_group) {
+              errors.push(`Assignment ${assignment} is a group and can't be added to another group.`);
+            } else if (obj.parent_assignment) {
+              errors.push(`Assignment ${assignment} is already part of another group.`);
+            } else {
+              let new_child = frappe.model.add_child(
+                doc,
+                'Topic Assignment Copy',
+                'grouped_assignments'
+              );
+              new_child.topic_assignment = assignment;
+              new_child.title = obj.title;
+              new_child.assignment_date = obj.assignment_date;
+            }
           }
         } catch (error) {
           console.error(`Error fetching assignment ${assignment}:`, error);
+          errors.push(`Error fetching assignment ${assignment}`);
         }
       }
+      if (errors.length > 0) {
+        frappe.msgprint({
+          title: __('Validation Errors'),
+          message: errors.join('<br>'),
+          indicator: 'red'
+        });
+        return;
+      }
+
       frappe.set_route('Form', 'Topic Assignment', doc.name);
-		});
-	},
+    });
+  },
 
   has_indicator_for_draft: 1,
   get_indicator: function (doc) {
