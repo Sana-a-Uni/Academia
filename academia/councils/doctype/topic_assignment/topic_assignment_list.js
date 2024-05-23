cfa58d971a453e11b62fd8c44a57d82a04e78191
Hamdi Mawari <almawryhmdyjmal@gmail.com>
2024-05-23 15:34:27 +0300

    feat(Councils): Add 'Add to Group' action to Topic Assignments list
    -  adding 'Add to Group' action to Topic Assignments list to provide secondary way to group "topic assignments".
    
    - Ensure that all selected topic assignments are added to the grouped assignment table before setting the route to the new Topic Assignment form.

--- academia/councils/doctype/topic_assignment/topic_assignment_list.js
+++ academia/councils/doctype/topic_assignment/topic_assignment_list.js
@@ -1,8 +1,40 @@
 frappe.listview_settings["Topic Assignment"] = {
 
   onload: function(topic_assignment) {
-		topic_assignment.page.add_inner_button(__("Add Group"), function() {
-			  alert("hello")
+
+    topic_assignment.page.add_action_item(__('Add to Group'), async function () {
+      const selected_assignments = topic_assignment.get_checked_items(true);
+      if (selected_assignments.length < 2) {
+        frappe.msgprint(__('Please select two or more Topic Assignments to add a group.'));
+        return;
+      }
+
+      await frappe.model.with_doctype('Topic Assignment');
+      var doc = frappe.model.get_new_doc('Topic Assignment');
+      doc.is_group = 1;
+
+      for (let assignment of selected_assignments) {
+        try {
+          let { message: obj } = await frappe.db.get_value(
+            "Topic Assignment",
+            assignment,
+            ["title", "assignment_date"]
+          );
+          if (obj) {
+            let new_child = frappe.model.add_child(
+              doc,
+              'Topic Assignment Copy',
+              'grouped_assignments'
+            );
+            new_child.topic_assignment = assignment;
+            new_child.title = obj.title;
+            new_child.assignment_date = obj.assignment_date;
+          }
+        } catch (error) {
+          console.error(`Error fetching assignment ${assignment}:`, error);
+        }
+      }
+      frappe.set_route('Form', 'Topic Assignment', doc.name);
 		});
 	},
 
