29bc5626c9eb74c4f8d89b25aa9c13756348c1cb
Ali Alnosairi <alialnosiari@gmail.com>
2024-05-27 19:25:12 +0300

    feat(councils):adding transaction link field in Topic Doctype

--- academia/councils/doctype/topic/topic.py
+++ academia/councils/doctype/topic/topic.py
@@ -1,10 +1,12 @@
 # Copyright (c) 2024, SanU and contributors
 # For license information, please see license.txt
 
+import json
+
 import frappe
 from frappe import _
 from frappe.model.document import Document
-import json
+
 
 class Topic(Document):
 	# begin: auto-generated types
@@ -13,9 +15,10 @@ class Topic(Document):
 	from typing import TYPE_CHECKING
 
 	if TYPE_CHECKING:
+		from frappe.types import DF
+
 		from academia.councils.doctype.topic_applicant.topic_applicant import TopicApplicant
 		from academia.councils.doctype.topic_attachment.topic_attachment import TopicAttachment
-		from frappe.types import DF
 
 		amended_from: DF.Link | None
 		applicants: DF.Table[TopicApplicant]
@@ -28,13 +31,14 @@ class Topic(Document):
 		title: DF.Data
 		topic_main_category: DF.Link
 		topic_sub_category: DF.Link
+		transaction: DF.Link | None
 	# end: auto-generated types
 	def onload(self):
 		if self.has_topic_assignment():
-			self.set_onload('has_topic_assignment', True)
+			self.set_onload("has_topic_assignment", True)
 		else:
-			self.set_onload('has_topic_assignment', False)
-		
+			self.set_onload("has_topic_assignment", False)
+
 	def validate(self):
 		self.validate_main_sub_category_relationship()
 		self.check_for_duplicate_applicants()
@@ -43,39 +47,41 @@ class Topic(Document):
 		"""
 		Validate the relationship between main category and sub-category, and check that the selected sub category is one of the  selected main category's sub categories
 		"""
-		if(self.topic_main_category and self.topic_sub_category):
-			main_category_of_selected_sub=frappe.get_value("Topic Sub Category",self.topic_sub_category,"main_category")
+		if self.topic_main_category and self.topic_sub_category:
+			main_category_of_selected_sub = frappe.get_value(
+				"Topic Sub Category", self.topic_sub_category, "main_category"
+			)
 			# Check if the main category of the sub-category does not  match the selected main category
-			if main_category_of_selected_sub!=self.topic_main_category:
+			if main_category_of_selected_sub != self.topic_main_category:
 				frappe.throw(
-					_("{0} is not sub category of {1}").format(self.topic_sub_category,self.topic_main_category)
+					_("{0} is not sub category of {1}").format(
+						self.topic_sub_category, self.topic_main_category
+					)
 				)
-		
-		
+
 	def check_for_duplicate_applicants(self):
 		"""
 		Ensures no duplicates in 'applicants' child table based on 'applicant' and 'applicant_type'.
 		Raises ValidationError if duplicates are found.
 		"""
-		applicants = self.get('applicants') or []  # Ensure it's always a list
+		applicants = self.get("applicants") or []  # Ensure it's always a list
 		seen_applicants = set()
 		for d in applicants:
 			applicant_id = (d.applicant, d.applicant_type)  # Tuple as unique identifier
 			if applicant_id in seen_applicants:
-				frappe.throw(f'Duplicate entry for Applicant: {d.applicant} of Type: {d.applicant_type}',
-							title='Duplicate Applicant Error')
-			seen_applicants.add(applicant_id)		
-			
+				frappe.throw(
+					f"Duplicate entry for Applicant: {d.applicant} of Type: {d.applicant_type}",
+					title="Duplicate Applicant Error",
+				)
+			seen_applicants.add(applicant_id)
+
 	def has_topic_assignment(self):
 		"""
 		Check if the topic has been assigned to a council
 		"""
-		assignments = frappe.get_all('Topic Assignment', 
-                                 filters={'topic': self.name}, 
-                                 fields=['name'])
+		assignments = frappe.get_all("Topic Assignment", filters={"topic": self.name}, fields=["name"])
 		if assignments:
 			return True
-
 
 
 @frappe.whitelist()
@@ -89,7 +95,7 @@ def get_all_related_assignments(topic_name):
 			ta.council ,
 			ta.assignment_date ,
 			ta.decision_type ,
-			ta.parent_assignment 
+			ta.parent_assignment
 		FROM
 			`tabTopic Assignment` ta
 		WHERE
@@ -119,6 +125,7 @@ def get_all_related_assignments(topic_name):
 	data = frappe.db.sql(query, (topic_name, topic_name), as_dict=True)
 	return data
 
+
 """	def check_duplicate_applicant(self):
 		if(self.applicants):
 			found=[]
@@ -130,23 +137,23 @@ def get_all_related_assignments(topic_name):
 					frappe.throw(
 						_("Applicant {0} entered before ").format(applicant.applicant_name)
 								)
-				found.append(user)   
-		
+				found.append(user)
+
 """
 
 # @frappe.whitelist()
 # def get_applicant_users_batch(applicants):
 #     """
 #     Retrieve the user associated with a batch of applicants.
-    
+
 #     Args:
 #         applicants (str): A JSON string representing a list of applicants where each applicant
 #                           is a dictionary with 'applicant_type' and 'applicant' keys.
-    
+
 #     Returns:
 #         list: A list of user values or None for each applicant in the batch.
 #     """
-	
+
 #     try:
 #         # Convert the JSON string back to a Python list of dictionaries
 #         applicants_list = json.loads(applicants)
