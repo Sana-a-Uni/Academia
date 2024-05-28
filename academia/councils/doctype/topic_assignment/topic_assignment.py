8613f808c7b8947398fc08709417e476aeac6a58
Ali Alnosairi <alialnosiari@gmail.com>
2024-05-29 02:56:39 +0300

    feat(councils): adding validation for parent assignments in Topic Assignment
    - Validate that the parent assignment is a group assignment
    - Ensure the parent assignment's council matches the current assignment's council
    - Check that the parent assignment is not submitted or canceled
    
    This validation improves data integrity and ensures consistent assignment linking.

--- academia/councils/doctype/topic_assignment/topic_assignment.py
+++ academia/councils/doctype/topic_assignment/topic_assignment.py
@@ -32,41 +32,47 @@ class TopicAssignment(Document):
 		sub_category: DF.Link | None
 		title: DF.Data
 		topic: DF.Link | None
+
 	# end: auto-generated types
 	def validate(self):
-		self.validate_topic_assignments()
 		if not self.get("__islocal") and self.is_group:
 			self.validate_grouped_assignments()
+		self.validate_parent_assignment()
 		self.ckeck_main_and_sub_categories()
-		self.validate_parent_assignment_docstatus()
 		self.validate_main_sub_category_relationship()
+
 	def autoname(self):
-		if (not self.is_group):
+		if not self.is_group:
 			# When there is a specific topic linked, include it in the name
-			self.name = frappe.model.naming.make_autoname(f'CNCL-TA-.YY.-.MM.-.{self.council}.-.###')
+			self.name = frappe.model.naming.make_autoname(f"CNCL-TA-.YY.-.MM.-.{self.council}.-.###")
 		else:
 			# For grouped assignments without a specific topic
-			self.name = frappe.model.naming.make_autoname(f'CNCL-TA-GRP-.YY.-.MM.-.{self.council}.-.###')
+			self.name = frappe.model.naming.make_autoname(f"CNCL-TA-GRP-.YY.-.MM.-.{self.council}.-.###")
+
 	def on_submit(self):
 		if self.is_group:
 			self.submit_grouped_assignments()
-
 
 	def validate_main_sub_category_relationship(self):
 		"""
 		Validate the relationship between main category and sub-category, and check that the selected sub category is one of the  selected main category's sub categories
 		"""
-		if(self.main_category and self.sub_category):
-			main_category_of_selected_sub=frappe.get_value("Topic Sub Category",self.sub_category,"main_category")
+		if self.main_category and self.sub_category:
+			main_category_of_selected_sub = frappe.get_value(
+				"Topic Sub Category", self.sub_category, "main_category"
+			)
 			# Check if the main category of the sub-category does not  match the selected main category
-			if main_category_of_selected_sub!=self.main_category:
+			if main_category_of_selected_sub != self.main_category:
 				frappe.throw(
-					_("{0} is not sub category of {1}").format(self.sub_category,self.main_category)
+					_("{0} is not sub category of {1}").format(self.sub_category, self.main_category)
 				)
+
 	def submit_grouped_assignments(self):
 		if self.decision_type in ["Resolved", "Transferred"]:
-			grouped_assignments = frappe.get_all("Topic Assignment",filters={"parent_assignment": self.name,"is_group":0}, fields=["name"])
-			if len(grouped_assignments)>0:
+			grouped_assignments = frappe.get_all(
+				"Topic Assignment", filters={"parent_assignment": self.name, "is_group": 0}, fields=["name"]
+			)
+			if len(grouped_assignments) > 0:
 				for assignment_data in grouped_assignments:
 					assignment = frappe.get_doc("Topic Assignment", assignment_data["name"])
 					assignment.decision_type = self.decision_type
@@ -74,24 +80,40 @@ class TopicAssignment(Document):
 					assignment.flags.ignore_validate = True
 					assignment.save(ignore_permissions=True)
 					assignment.submit()
-     
-     
-	def validate_parent_assignment_docstatus(self):
-		if (self.parent_assignment):
-			parent_assignment = frappe.get_doc("Topic Assignment", self.parent_assignment)
-			if(parent_assignment.docstatus):
-				frappe.throw(_(f"""the chosen Parent Assignment {parent_assignment.name} is
-					{("submitted" if parent_assignment.docstatus == 1 else "Canceled" )}"""
-				))
 
-			
-            
+	def validate_parent_assignment(self):
+		"""
+		Validates the parent assignment of the current Topic Assignment.
+
+		Ensures that:
+		1. The parent assignment is a group assignment.
+		2. The parent assignment's council matches the current assignment's council.
+		3. The parent assignment is not submitted or canceled.
+		"""
+		if self.parent_assignment:
+			parent_assignment = frappe.db.get_value(
+				"Topic Assignment", self.parent_assignment, ["council", "is_group", "docstatus"], as_dict=1
+			)
+			if parent_assignment["is_group"] == 1:  # is group
+				if parent_assignment["council"] != self.council:
+					frappe.throw(
+						_(
+							"The council of the parent assignment does not match the current assignment's council."
+						)
+					)
+				if parent_assignment["docstatus"] != 0:
+					status = "submitted" if parent_assignment["docstatus"] == 1 else "canceled"
+					frappe.throw(
+						_(f"The chosen parent assignment {self.parent_assignment} cannot be {status}.")
+					)
+			else:
+				frappe.throw(_("The chosen parent assignment is not a group assignment."))
 
 	def ckeck_main_and_sub_categories(self):
 		"""
-    	Validates that both main_category and sub_category fields are set.
-    	Throws an error if any of these fields are missing.
-    	"""
+		Validates that both main_category and sub_category fields are set.
+		Throws an error if any of these fields are missing.
+		"""
 		if not self.main_category:
 			frappe.throw(_("Main category must be set."))
 		elif not self.sub_category:
@@ -99,29 +121,32 @@ class TopicAssignment(Document):
 
 	def validate_grouped_assignments(self):
 		"""
-        Validates that all grouped Topic Assignments have the same council
-        as the current group Topic Assignment and are not group assignments.
-        """
+		Validates that all grouped Topic Assignments have the same council
+		as the current group Topic Assignment and are not group assignments.
+		"""
 		grouped_assignments = frappe.get_all(
-            "Topic Assignment",
-            filters={"parent_assignment": self.name},
-            fields=["name", "council","is_group"]
-        )
+			"Topic Assignment",
+			filters={"parent_assignment": self.name},
+			fields=["name", "council", "is_group"],
+		)
 
 		for assignment in grouped_assignments:
 			if assignment.council != self.council:
 				frappe.throw(
-                    _("Grouped topic assignment {0} does not have the same council as the group assignment.").format(assignment.name)
-                )
+					_(
+						"Grouped topic assignment {0} does not have the same council as the group assignment."
+					).format(assignment.name)
+				)
 			if assignment.is_group:
 				frappe.throw(
 					_("Grouped topic assignment {0} can not be of type group.").format(assignment.name)
 				)
 
+
 @frappe.whitelist()
 def get_available_topics(doctype, txt, searchfield, start, page_len, filters):
-    return frappe.db.sql(
-        """SELECT name FROM `tabTopic`
+	return frappe.db.sql(
+		"""SELECT name FROM `tabTopic`
         WHERE docstatus= %(docstatus)s  AND
         status IN %(status)s AND
         name NOT IN (
@@ -131,31 +156,22 @@ def get_available_topics(doctype, txt, searchfield, start, page_len, filters):
         )
         ORDER BY name
        """,
-        {
-            "council": filters.get("council"),
-            "docstatus":filters.get("docstatus"),
-            "status":filters.get("status")
-
-        },
-        as_list=True
-    )
+		{
+			"council": filters.get("council"),
+			"docstatus": filters.get("docstatus"),
+			"status": filters.get("status"),
+		},
+		as_list=True,
+	)
 
 
 @frappe.whitelist()
 def get_grouped_assignments(parent_name):
 	grouped_assignments = frappe.get_all(
 		"Topic Assignment",
-  		filters={
-        	"parent_assignment": parent_name,
-			"is_group":0
-		},
-  		fields=[
-			"name",
-			"title",
-			"assignment_date",
-			"decision_type"
-        ]
-    )
+		filters={"parent_assignment": parent_name, "is_group": 0},
+		fields=["name", "title", "assignment_date", "decision_type"],
+	)
 	return grouped_assignments
 
 
@@ -165,13 +181,13 @@ def add_assignment_to_group(parent_name, assignment_name):
 		assignment = frappe.get_doc("Topic Assignment", assignment_name)
 
 		assignment.parent_assignment = parent_name
-		
+
 		assignment.save()
-		
+
 		return "ok"
 	except Exception as e:
 		frappe.log_error(frappe.get_traceback(), "Error adding assignment")
-		return  str(e)
+		return str(e)
 
 
 @frappe.whitelist()
@@ -180,10 +196,10 @@ def delete_assignment_from_group(assignment_name):
 		assignment = frappe.get_doc("Topic Assignment", assignment_name)
 
 		assignment.parent_assignment = None
-		
+
 		assignment.save()
-		
+
 		return "ok"
 	except Exception as e:
 		frappe.log_error(frappe.get_traceback(), "Error removing assignment")
-		return  str(e)
+		return str(e)
