983a0d0f793ce2221b4decdfdcaf5989777685b6
HalaAlafoori <halaalafoori@gmail.com>
2024-06-03 21:04:54 +0530

    feat(Transaction Management): add Transaction Categoty Template doctype

--- academia/transaction_management/doctype/external_entity/external_entity.py
+++ academia/transaction_management/doctype/external_entity/external_entity.py
@@ -6,4 +6,19 @@ from frappe.model.document import Document
 
 
 class ExternalEntity(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from academia.transaction_management.doctype.external_entity_designation.external_entity_designation import ExternalEntityDesignation
+		from frappe.types import DF
+
+		company: DF.Link | None
+		designation: DF.Table[ExternalEntityDesignation]
+		external_entity: DF.Data | None
+		is_group: DF.Check
+		parent_external_entity: DF.Link | None
+	# end: auto-generated types
 	pass
