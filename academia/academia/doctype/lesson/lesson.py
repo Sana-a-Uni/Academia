520f20f9e6c44285abdaf5edd67a251332557a9c
Mohammed Altheeb <altheebinfo@gmail.com>
2024-06-06 02:44:32 +0300

    refact(core): Refactoring and Add Some Fields in Lesson Doctype

--- academia/academia/doctype/lesson/lesson.py
+++ academia/academia/doctype/lesson/lesson.py
@@ -17,10 +17,17 @@ class Lesson(Document):
 		academic_term: DF.Link
 		academic_year: DF.Link
 		course: DF.Link
+		date: DF.Date
+		faculty: DF.Link
+		from_time: DF.Time
 		group: DF.Data | None
 		instructor: DF.Link
+		lesson_type: DF.Literal["", "Ordinary Lesson", "Compensatory Lesson"]
 		level: DF.Link
 		program: DF.Link
 		room: DF.Link
+		schedule_template: DF.ReadOnly | None
+		schedule_template_version: DF.Link
+		to_time: DF.Time
 	# end: auto-generated types
 	pass
