3719fba9cc5cdd19a0095c9f05969cce0f8c0bd0
Ali Alnosairi <alialnosiari@gmail.com>
2024-06-08 03:00:17 +0300

    chore(councils): rename variables and  methods to match the new name of doctype "Topic "

--- academia/councils/doctype/session_topic/session_topic.py
+++ academia/councils/doctype/session_topic/session_topic.py
@@ -6,29 +6,29 @@ from frappe.model.document import Document
 
 
 class SessionTopic(Document):
-    # begin: auto-generated types
-    # This code is auto-generated. Do not modify anything in this block.
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
 
-    from typing import TYPE_CHECKING
+	from typing import TYPE_CHECKING
 
-    if TYPE_CHECKING:
-        from frappe.types import DF
+	if TYPE_CHECKING:
+		from frappe.types import DF
 
-        decision: DF.TextEditor | None
-        decision_type: DF.Literal["", "Postponed", "Resolved", "Transferred"]
-        description: DF.TextEditor | None
-        parent: DF.Data
-        parentfield: DF.Data
-        parenttype: DF.Data
-        title: DF.Data | None
-        topic_assignment: DF.Link
-    # end: auto-generated types
-    pass
+		decision: DF.TextEditor | None
+		decision_type: DF.Literal["", "Postponed", "Resolved", "Transferred"]
+		description: DF.TextEditor | None
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+		title: DF.Data | None
+		topic: DF.Link
+	# end: auto-generated types
+	pass
 
-    @property
-    def description(self):
-        return frappe.get_value('Topic Assignment', self.topic_assignment, 'description')
+	@property
+	def description(self):
+		return frappe.get_value("Topic", self.topic, "description")
 
-    @property
-    def title(self):
-        return frappe.get_value('Topic Assignment', self.topic_assignment, 'title')
+	@property
+	def title(self):
+		return frappe.get_value("Topic", self.topic, "title")
