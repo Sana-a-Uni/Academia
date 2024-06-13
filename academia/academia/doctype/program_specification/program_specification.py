85aa7ad58a475a01b9895bbd7090c35320b5a6d6
Mohammed Altheeb <altheebinfo@gmail.com>
2024-06-13 07:16:14 +0300

    fix(academia): Fix a TabError: Inconsistent Use of Tabs and Spaces in Indentation

--- academia/academia/doctype/program_specification/program_specification.py
+++ academia/academia/doctype/program_specification/program_specification.py
@@ -4,43 +4,39 @@
 import frappe
 from frappe.model.document import Document
 
-
 class ProgramSpecification(Document):
     def get_course_list(self):
         return [frappe.get_doc("Course Specification", study_plan_course.course_code) for study_plan_course in self.table_ytno]
 
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
 
+    from typing import TYPE_CHECKING
 
+    if TYPE_CHECKING:
+        from academia.academia.doctype.course_language.course_language import CourseLanguage
+        from academia.academia.doctype.study_plan_course.study_plan_course import StudyPlanCourse
+        from frappe.types import DF
 
-	# begin: auto-generated types
-	# This code is auto-generated. Do not modify anything in this block.
-
-	from typing import TYPE_CHECKING
-
-	if TYPE_CHECKING:
-		from academia.academia.doctype.course_language.course_language import CourseLanguage
-		from academia.academia.doctype.study_plan_course.study_plan_course import StudyPlanCourse
-		from frappe.types import DF
-
-		approval_date: DF.Date | None
-		courses: DF.Check
-		date_of_programe_development: DF.Date | None
-		delivery_mode: DF.Literal["", "On Campus", "Online"]
-		effective_year: DF.Link | None
-		faculty: DF.Link
-		faculty_department: DF.Link
-		file: DF.Attach | None
-		maximum_research_period: DF.Int
-		minimum_course_average_to_start_research: DF.Data | None
-		minimum_research_period: DF.Int
-		program_abbreviation: DF.Data
-		program_degree: DF.Data
-		program_name: DF.Link
-		research_or_thesis: DF.Check
-		starrting_year_of_the_program: DF.Date | None
-		study_language: DF.TableMultiSelect[CourseLanguage]
-		study_methods: DF.Link | None
-		table_ytno: DF.Table[StudyPlanCourse]
-		total_hours_required_to_award_degree: DF.Int
-	# end: auto-generated types
-	pass
+        approval_date: DF.Date | None
+        courses: DF.Check
+        date_of_programe_development: DF.Date | None
+        delivery_mode: DF.Literal["", "On Campus", "Online"]
+        effective_year: DF.Link | None
+        faculty: DF.Link
+        faculty_department: DF.Link
+        file: DF.Attach | None
+        maximum_research_period: DF.Int
+        minimum_course_average_to_start_research: DF.Data | None
+        minimum_research_period: DF.Int
+        program_abbreviation: DF.Data
+        program_degree: DF.Data
+        program_name: DF.Link
+        research_or_thesis: DF.Check
+        starrting_year_of_the_program: DF.Date | None
+        study_language: DF.TableMultiSelect[CourseLanguage]
+        study_methods: DF.Link | None
+        table_ytno: DF.Table[StudyPlanCourse]
+        total_hours_required_to_award_degree: DF.Int
+    # end: auto-generated types
+    pass
--- academia/academia/doctype/program_specification/test_program_specification.py
+++ academia/academia/doctype/program_specification/test_program_specification.py
@@ -5,15 +5,12 @@ import frappe
 from frappe.tests.utils import FrappeTestCase
 
 test_data = {
-
     "program_name": "_Test Program",
     "description": "_Test Program",
     "course_code": [
-
         {
             "course_name": "_Test Course 1",
         },
-
         {
             "course_name": "_Test Course 2",
         },
@@ -21,12 +18,10 @@ test_data = {
 }
 
 class TestProgramSpecification(FrappeTestCase):
-	
-	def setUp(self):
+    def setUp(self):
         make_program_and_linked_courses(
             "_Test Program 1", ["_Test Course 1", "_Test Course 2"]
         )
-
 
     def test_get_course_list(self):
         program = frappe.get_doc("Program Specification", "_Test Program 1")
@@ -35,12 +30,10 @@ class TestProgramSpecification(FrappeTestCase):
         self.assertEqual(courses[1].name, "_Test Course 2")
         frappe.db.rollback()
 
-
-	def tearDown(self):
+    def tearDown(self):
         for dt in ["Program Specification", "Course Specification"]:
             for entry in frappe.get_all(dt):
                 frappe.delete_doc(dt, entry.name)
-
 
 def make_program(name):
     program = frappe.get_doc(
@@ -51,25 +44,19 @@ def make_program(name):
             "description": "_test description",
             "is_featured": True,
         }
-
     ).insert()
-
     return program.name
 
 def make_program_and_linked_courses(program_name, course_name_list):
-
     try:
         program = frappe.get_doc("Program Specification", program_name)
-
     except frappe.DoesNotExistError:
         make_program(program_name)
         program = frappe.get_doc("Program Specification", program_name)
     course_list = [make_course(course_name) for course_name in course_name_list]
-
     for course in course_list:
         program.append("table_ytno", {"course_code": course, "required": 1})
-    
-	program.save()
+    program.save()
     return program
 
 def make_course(name):
@@ -79,22 +66,17 @@ def make_course(name):
             "course_name": name,
             "course_code": name,
         }
-
     ).insert()
     return course.name
 
 def setup_program():
-
     all_courses_list = [
         {"course_code": course["course_name"]}
         for course in test_data["course_code"]
     ]
-
     for course in all_courses_list:
         make_course(course["course_code"])
 
     course_list = [course["course_name"] for course in test_data["course_code"]]
-
     program = make_program_and_linked_courses(test_data["program_name"], course_list)
-	
     return program
