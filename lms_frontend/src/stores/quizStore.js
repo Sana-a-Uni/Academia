428f97a10be6115b08685fdc3b05dee29f15c547
xair <aldubai.atrab@gmail.com>
2024-06-14 19:32:15 +0300

    feat(LMS): Add fetchQuiz to quizStore to fetch quiz from frappe API

--- lms_frontend/src/stores/quizStore.js
+++ lms_frontend/src/stores/quizStore.js
@@ -4,6 +4,7 @@ import axios from "axios";
 export const useQuizStore = defineStore("quiz", {
 	state: () => ({
 		quizzes: [],
+		quiz: {},
 		quizInstructions: [],
 		loading: false,
 		error: null,
@@ -46,6 +47,32 @@ export const useQuizStore = defineStore("quiz", {
 				this.quizInstructions = response.data.data;
 			} catch (error) {
 				this.error = error.message || "An error occurred while fetching quiz instructions.";
+			} finally {
+				this.loading = false;
+			}
+		},
+
+		async fetchQuiz(quizName) {
+			this.loading = true;
+			this.error = null;
+			try {
+				const response = await axios.get(
+					`http://localhost:8080/api/method/academia.lms_api.student.quiz.quiz.get_quiz`,
+					{
+						params: {
+							quiz_name: quizName,
+						},
+						headers: {
+							Authorization: "Bearer 0b88a69d4861506:536f9ea01265e49",
+						},
+					}
+				);
+
+				this.quiz = response.data.data;
+				console.log(response.data.data);
+			} catch (error) {
+				this.error = error.message || "An error occurred while fetching the quiz.";
+				console.log("errrrrrrrrrrror");
 			} finally {
 				this.loading = false;
 			}
