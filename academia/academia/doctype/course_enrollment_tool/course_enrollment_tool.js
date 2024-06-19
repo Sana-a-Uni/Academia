e33df8eeaeec12f16f30b266202191267968c461
Ahmed Fakher <sahebalwait@gmail.com>
2024-06-19 22:27:25 +0300

    refact(core): refactor the course enrollment tool.js to retrive the students.

--- academia/academia/doctype/course_enrollment_tool/course_enrollment_tool.js
+++ academia/academia/doctype/course_enrollment_tool/course_enrollment_tool.js
@@ -4,12 +4,14 @@
 frappe.ui.form.on('Course Enrollment Tool', {
     "get_courses": function(frm) {
 		frm.set_value("courses",[]);
+        frm.set_value("students",[]);
 		frappe.call({
 			method: "get_courses",
 			doc:frm.doc,
 			callback: function(r) {
 				if(r.message) {
-					frm.set_value("courses", r.message);
+					frm.set_value("courses", r.message.courses);
+                    frm.set_value("students", r.message.students);
 				}
 			}
 		});
