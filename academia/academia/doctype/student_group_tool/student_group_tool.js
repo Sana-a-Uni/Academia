a16127b97914d5b10032cd6d433a6b8d7ec8da70
Ahmed Fakher <sahebalwait@gmail.com>
2024-06-23 03:00:41 +0300

    feat(core): connect the get students and generate groups buttons to the py file.

--- academia/academia/doctype/student_group_tool/student_group_tool.js
+++ academia/academia/doctype/student_group_tool/student_group_tool.js
@@ -1,8 +1,29 @@
 // Copyright (c) 2024, SanU and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Student Group Tool", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('Student Group Tool', {
+    "get_students": function(frm) {
+        frm.set_value("students",[]);
+		frappe.call({
+			method: "get_students",
+			doc:frm.doc,
+			callback: function(r) {
+				if(r.message) {
+					frm.set_value("students", r.message);
+				}
+			}
+		});
+	},
+	"generate_groups": function(frm) {
+		//frm.set_value("courses",[]);
+		frappe.call({
+			method: "generate_groups",
+			doc:frm.doc,
+			callback: function(r) {
+				if(r.message) {
+					//frm.set_value("courses", r.message);
+				}
+			}
+		});
+	}
+});
