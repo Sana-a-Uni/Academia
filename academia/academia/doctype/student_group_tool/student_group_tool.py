9479500a3101b41d95cc7aa094d8488054292863
Ahmed Fakher <sahebalwait@gmail.com>
2024-06-23 03:03:48 +0300

    feat(core): enable the get students button to get the students from the program enrollment doctype.

--- academia/academia/doctype/student_group_tool/student_group_tool.py
+++ academia/academia/doctype/student_group_tool/student_group_tool.py
@@ -1,8 +1,10 @@
 # Copyright (c) 2024, SanU and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
+from frappe.utils import cint
 
 
 class StudentGroupTool(Document):
@@ -20,7 +22,44 @@ class StudentGroupTool(Document):
 		faculity: DF.Link
 		program: DF.Link
 		student_batch: DF.Link
-		table_yldo: DF.Table[StudentGroupStudent]
+		students: DF.Table[StudentGroupStudent]
 		tolerance: DF.Int
 	# end: auto-generated types
-	pass
+	
+	@frappe.whitelist()
+	def get_students(self):
+
+		# doctype = frappe.get_doc('DocType', 'Student Group Tool')
+		# doctype.append('fields', {
+		# 	'fieldname': 'ssssss',
+		# 	'fieldtype': 'Data',
+		# 	'label': 'ssssss'
+		# })
+		# doctype.save()
+		# frappe.db.commit()
+		students = []
+
+		if not self.based_on:
+			frappe.throw(_("Mandatory field - Based On"))
+		elif not self.program:
+			frappe.throw(_("Mandatory field - Program"))
+		elif not self.student_batch:
+			frappe.throw(_("Mandatory field - Student Batch"))
+		elif not self.capacity:
+			frappe.throw(_("Mandatory field - Capacity"))
+		elif not self.tolerance:
+			frappe.throw(_("Mandatory field - Tolerance"))
+		else:
+			program_enrollment = frappe.get_list('Program Enrollment', fields='*')
+			for student in program_enrollment:
+				if student['program'] == self.program and student['student_batch'] == self.student_batch:
+					students.append(student)
+		
+		if not students:
+			frappe.throw(_("No students Found"))
+		else:
+			return students
+
+	@frappe.whitelist()
+	def generate_groups(self):
+		
