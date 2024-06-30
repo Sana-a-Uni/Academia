76f41e1d3bfdf989b6194e71fb3735f1e2cff0af
xair <aldubai.atrab@gmail.com>
2024-07-01 01:13:17 +0300

    feat(LMS): Add some package

--- lms_frontend/src/main.js
+++ lms_frontend/src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
 import { QuillEditor } from "@vueup/vue-quill";
+import VTooltip from "v-tooltip";
 
 import "@vueup/vue-quill/dist/vue-quill.snow.css";
 
@@ -10,6 +11,8 @@ const pinia = createPinia();
 
 const app = createApp(App);
 app.use(pinia);
+app.use(VTooltip);
+
 app.use(router);
 app.component("QuillEditor", QuillEditor);
 app.mount("#app");
