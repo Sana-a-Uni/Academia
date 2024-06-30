033ebf75c70d36cd9d620283689c45dbd674b1e1
xair <aldubai.atrab@gmail.com>
2024-07-01 01:36:57 +0300

    style(LMS): Add studentDashboard and courseView page

---
+++ lms_frontend/src/views/studentDashboard/dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="layout">
+		<navbar />
+		<dashboard />
+	</div>
+</template>
+
+<script setup>
+import navbar from "@/components/Navbar.vue";
+import dashboard from "@/components/dashboard.vue";
+</script>
+
+<style>
+/* Your styles for the layout */
+.layout {
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+	background-color: #f0f5f9;
+}
+</style>
