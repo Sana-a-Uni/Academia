b0ed9bf1da8d85953aa3f326293fde18d13b494b
Ahmed Fakher <sahebalwait@gmail.com>
2024-07-03 20:28:05 +0300

    feat(core): validate the days checkboxes to can only chose one.

--- academia/academia/doctype/lesson_template/lesson_template.js
+++ academia/academia/doctype/lesson_template/lesson_template.js
@@ -1,8 +1,35 @@
 // Copyright (c) 2024, SanU and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Lesson Template", {
-// 	refresh(frm) {
+function handle_checkboxes(frm, selected_fieldname){
+    const checkbox_fields = [
+        'saturday',
+        'sunday',
+        'monday',
+        'tuesday',
+        'wednesday',
+        'thursday'
+    ];
 
-// 	},
-// });
+    checkbox_fields.forEach(fieldname => {
+        if (fieldname !== selected_fieldname){
+            frm.set_value(fieldname, 0);
+        }
+    });
+}
+
+frappe.ui.form.on("Lesson Template", {
+	refresh(frm) {
+
+	},
+    saturday: function(frm){
+        if (frm.doc.saturday) {
+            handle_checkboxes(frm, 'saturday');
+        }
+    },
+    sunday: function(frm){
+        if (frm.doc.sunday) {
+            handle_checkboxes(frm, 'sunday');
+        }
+    }
+});
