163b5c038862d16e61cc83d5c519088a7ff76a30
Ahmed Fakher <sahebalwait@gmail.com>
2024-07-03 18:02:50 +0300

    feat(core): add the days to the lesson template doctype.

--- academia/academia/doctype/lesson_template/lesson_template.py
+++ academia/academia/doctype/lesson_template/lesson_template.py
@@ -25,11 +25,17 @@ class LessonTemplate(Document):
 		is_multy_group: DF.Check
 		lesson_type: DF.Literal["", "Ordinary Lesson", "Compensatory Lesson"]
 		level: DF.Link
+		monday: DF.Check
 		program: DF.Link
 		room: DF.Link
+		saturday: DF.Check
 		schedule_template: DF.ReadOnly | None
 		schedule_template_version: DF.Link
+		sunday: DF.Check
 		table_ocar: DF.Table[MultyLessonTemplate]
+		thursday: DF.Check
 		to_time: DF.Time
+		tuesday: DF.Check
+		wednesday: DF.Check
 	# end: auto-generated types
 	pass
