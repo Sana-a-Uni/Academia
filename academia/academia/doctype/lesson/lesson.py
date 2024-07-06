044b39efc688b8f075ae86302641c2cc9f613dd8
Akram Al-Hajari <mkozxcasdqwe123123@gmail.com>
2024-07-06 18:35:13 +0300

    fix(core): rename some fields in Lesson Doctype

--- academia/academia/doctype/lesson/lesson.py
+++ academia/academia/doctype/lesson/lesson.py
@@ -12,7 +12,7 @@ class Lesson(Document):
 	from typing import TYPE_CHECKING
 
 	if TYPE_CHECKING:
-		from academia.academia.doctype.multy_lesson_template.multy_lesson_template import MultyLessonTemplate
+		from academia.academia.doctype.multi_lesson_template.multi_lesson_template import MultiLessonTemplate
 		from frappe.types import DF
 
 		academic_term: DF.Link
@@ -24,7 +24,7 @@ class Lesson(Document):
 		from_time: DF.Time
 		group: DF.Link | None
 		instructor: DF.Link
-		is_multy_group: DF.Check
+		is_multi_group: DF.Check
 		lesson_type: DF.Literal["", "Ordinary Lesson", "Compensatory Lesson"]
 		level: DF.Link
 		program: DF.Link
@@ -32,7 +32,7 @@ class Lesson(Document):
 		schedule_template: DF.ReadOnly | None
 		schedule_template_version: DF.Link
 		sub_group: DF.Data | None
-		table_hfgk: DF.Table[MultyLessonTemplate]
+		table_hfgk: DF.Table[MultiLessonTemplate]
 		to_time: DF.Time
 	# end: auto-generated types
 	pass
