211d2bca82753a06cf7f4b27f5bb800e5c6432b4
Ali Alnosairi <alialnosiari@gmail.com>
2024-07-08 18:55:22 +0300

    fix(councils): fixing translatable strings using positional formatters in council doctype

--- academia/councils/doctype/council/council.py
+++ academia/councils/doctype/council/council.py
@@ -7,52 +7,48 @@ from frappe.model.document import Document
 
 
 class Council(Document):
-    # begin: auto-generated types
-    # This code is auto-generated. Do not modify anything in this block.
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
 
-    from typing import TYPE_CHECKING
+	from typing import TYPE_CHECKING
 
-    if TYPE_CHECKING:
-        from academia.councils.doctype.council_member.council_member import CouncilMember
-        from frappe.types import DF
+	if TYPE_CHECKING:
+		from academia.councils.doctype.council_member.council_member import CouncilMember
+		from frappe.types import DF
 
-        administrative_body: DF.Link
-        company: DF.Link
-        council_name: DF.Data
-        get_members_from_other_companies: DF.Check
-        members: DF.Table[CouncilMember]
-    # end: auto-generated types
- 
-    def validate(self):
-        validate_members(self.members)
+		administrative_body: DF.Link
+		company: DF.Link
+		council_name: DF.Data
+		get_members_from_other_companies: DF.Check
+		members: DF.Table[CouncilMember]
+	# end: auto-generated types
+
+	def validate(self):
+		validate_members(self.members)
 
 
 def validate_members(members):
-    roles, employees = zip(*[(member.member_role, member.employee)
-                         for member in members])
-    check_employee_duplicate(employees)
-    check_council_head_and_reporter_duplication(roles)
-    # check if at least one member is a coucil head
-    if roles.count(_('Council Head')) < 1:
-        frappe.throw(_(f"This Council doesn't have a Council Head"))
+	roles, employees = zip(*[(member.member_role, member.employee) for member in members])
+	check_employee_duplicate(employees)
+	check_council_head_and_reporter_duplication(roles)
+	# check if at least one member is a coucil head
+	if roles.count(_("Council Head")) < 1:
+		frappe.throw(_("This Council doesn't have a Council Head"))
 
 
 # Function to check for member duplicates
 def check_employee_duplicate(employees):
-    # Iterate through each member
-    for employee in employees:
-        # Check if the member count is greater than 1, indicating a duplicate
-        if employees.count(employee) > 1:
-            # Throw an error if duplicates are found
-            frappe.throw(_(f"Council members can't be duplicated"))
-            
+	# Iterate through each member
+	for employee in employees:
+		# Check if the member count is greater than 1, indicating a duplicate
+		if employees.count(employee) > 1:
+			# Throw an error if duplicates are found
+			frappe.throw(_("Council members can't be duplicated"))
+
+
 def check_council_head_and_reporter_duplication(roles):
-    # Check if the Council Head count is greater than 1, indicating a duplicate
-    if roles.count(_('Council Head')) > 1:
-        frappe.throw(
-            _(f"This Council already has a {_('Council Head')}"))
-    elif roles.count(_('Council Reporter')) > 1:
-        frappe.throw(
-            _(f"This Council already has a {_('Council Reporter')}"))
-            
-        
+	# Check if the Council Head count is greater than 1, indicating a duplicate
+	if roles.count(_("Council Head")) > 1:
+		frappe.throw(_("This Council already has a Council Head"))
+	elif roles.count(_("Council Reporter")) > 1:
+		frappe.throw(_("This Council already has a Council Reporter"))
