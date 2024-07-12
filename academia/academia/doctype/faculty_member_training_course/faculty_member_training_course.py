38355daca3a43f0aec9bfad21f382af822979922
eng-AmaniMo <eng.amani99@hotmail.com>
2024-07-12 17:24:11 +0300

    feat: Adding initial Arabic translation file (ar.csv)

--- academia/academia/doctype/faculty_member_training_course/faculty_member_training_course.py
+++ academia/academia/doctype/faculty_member_training_course/faculty_member_training_course.py
@@ -8,6 +8,25 @@ from frappe import _
 
 
 class FacultyMemberTrainingCourse(Document):
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from frappe.types import DF
+
+        as_a_trainer: DF.Check
+        certification: DF.Attach | None
+        course_name: DF.Data
+        ends_on: DF.Date | None
+        location: DF.Data | None
+        name: DF.Int | None
+        parent: DF.Data
+        parentfield: DF.Data
+        parenttype: DF.Data
+        starts_on: DF.Date | None
+    # end: auto-generated types
     def validate(self):
         if self.starts_on and self.ends_on:
             if self.ends_on <= self.starts_on:
