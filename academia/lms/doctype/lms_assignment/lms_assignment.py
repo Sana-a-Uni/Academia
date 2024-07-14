aa1807f032105862d531eb906f8dcd5a8a33a709
xair <aldubai.atrab@gmail.com>
2024-07-15 00:53:18 +0300

    refact(LMS): Remove some fields from LMS Assignment Doctype

--- academia/lms/doctype/lms_assignment/lms_assignment.py
+++ academia/lms/doctype/lms_assignment/lms_assignment.py
@@ -6,4 +6,22 @@ from frappe.model.document import Document
 
 
 class LMSAssignment(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		attachment: DF.Attach | None
+		course: DF.Link | None
+		from_date: DF.Datetime | None
+		instruction: DF.SmallText | None
+		make_the_assignment_availability: DF.Check
+		question: DF.TextEditor | None
+		student_group: DF.Link | None
+		title: DF.Data
+		to_date: DF.Datetime | None
+	# end: auto-generated types
 	pass
