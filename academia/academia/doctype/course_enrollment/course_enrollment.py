5d5700a59f6a6eae47b05bde82abf8fa1c3e8b23
Mohammed Altheeb <midoaltheeb@gmail.com>
2024-07-27 02:57:12 +0300

    refactor(academia): refactoring for course enrollment doctype

--- academia/academia/doctype/course_enrollment/course_enrollment.py
+++ academia/academia/doctype/course_enrollment/course_enrollment.py
@@ -6,4 +6,23 @@ from frappe.model.document import Document
 
 
 class CourseEnrollment(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		academic_program: DF.Link
+		academic_term: DF.Link | None
+		academic_year: DF.Link
+		course: DF.Link | None
+		enrollment_date: DF.Date | None
+		faculty: DF.Link | None
+		faculty_department: DF.Link | None
+		student: DF.Link
+		student_batch: DF.Link
+		student_name: DF.ReadOnly | None
+	# end: auto-generated types
 	pass
