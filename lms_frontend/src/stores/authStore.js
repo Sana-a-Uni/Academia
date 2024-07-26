150a85605c8be1be9745aaa6cbc6435dc041bd23
xair <aldubai.atrab@gmail.com>
2024-07-26 23:41:17 +0300

    feat(LMS): save token and role in cookies

--- lms_frontend/src/stores/authStore.js
+++ lms_frontend/src/stores/authStore.js
@@ -1,12 +1,11 @@
-// src/stores/authStore.js
 import { defineStore } from "pinia";
 import axios from "axios";
+import Cookies from "js-cookie";
+import router from "../router";
 
 export const useAuthStore = defineStore("auth", {
 	state: () => ({
 		user: null,
-		apiKey: "",
-		apiSecret: "",
 		userDetails: {},
 		userRoles: [],
 		error: null,
@@ -16,27 +15,31 @@ export const useAuthStore = defineStore("auth", {
 			try {
 				const response = await axios.post(
 					"http://localhost:8080/api/method/academia.lms_api.login.login",
-					{
-						username,
-						password,
-					}
+					{ username, password }
 				);
-				if (response.data.message === true) {
-					this.apiKey = response.data.key_details.api_key;
-					this.apiSecret = response.data.key_details.api_secret;
-					this.userDetails = response.data.user_details;
-					this.userRoles = response.data.user_role;
-					this.user =
-						response.data.user_details[0].first_name +
-						" " +
-						response.data.user_details[0].last_name;
-					this.error = null;
+
+				if (response.data && response.data.message === true) {
+					const { user_details, user_role, key_details } = response.data;
+
+					if (user_details && user_role && key_details) {
+						this.userDetails = user_details;
+						this.userRoles = user_role;
+						this.user = `${user_details[0].first_name} ${user_details[0].last_name}`;
+						this.error = null;
+
+						const token = `token ${key_details.api_key}:${key_details.api_secret}`;
+						Cookies.set("authToken", token, { expires: 7 });
+						Cookies.set("role", this.userRoles[0], { expires: 7 });
+					} else {
+						this.error = "Incomplete data received from server";
+					}
 				} else {
 					this.error = "Login failed";
 				}
 			} catch (error) {
-				this.error = error.response.data.message || "An error occurred during login";
+				this.error = error.response?.data?.message || "An error occurred during login";
 			}
 		},
+		
 	},
 });
