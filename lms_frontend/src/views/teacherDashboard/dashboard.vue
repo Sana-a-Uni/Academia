fbee6dfdf175ccc06a78ba1ce9144f2b59db9c48
xair <aldubai.atrab@gmail.com>
2024-07-27 01:33:48 +0300

    style(LMS): Add teacher dashboard

---
+++ lms_frontend/src/views/teacherDashboard/dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="layout">
+		<navbar />
+		<dashboard />
+	</div>
+</template>
+
+<script setup>
+import navbar from "@/components/teacherComponents/layout/Navbar.vue";
+import dashboard from "@/components/teacherComponents/dashboard/dashboard.vue";
+</script>
+
+<style>
+.layout {
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+	background-color: #f0f5f9;
+}
+</style>
