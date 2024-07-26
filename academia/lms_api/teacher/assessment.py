30a4b5a2290a36abec3dd8968a2c13ddc6c7c65d
xair <aldubai.atrab@gmail.com>
2024-07-26 04:08:54 +0300

    feat(LMS): Add get_assignment_details function

--- academia/lms_api/teacher/assessment.py
+++ academia/lms_api/teacher/assessment.py
@@ -27,3 +27,32 @@ def get_submitted_assignments_by_faculty_member(faculty_member_id= "ACAD-FM-0000
     # إعداد الاستجابة
     frappe.response["status_code"] = 200
     frappe.response["submitted_assignments"] = submitted_assignments
+
+
+@frappe.whitelist(allow_guest=True)
+def get_assignment_details(assignment_submission_id="20817d93e8"):
+    if not assignment_submission_id:
+        frappe.throw(_("Assignment Submission ID is required"))
+
+    submission = frappe.get_doc('Assignment Submission', assignment_submission_id)
+    
+    assignment = frappe.get_doc('LMS Assignment', submission.assignment)
+    
+    student = frappe.get_doc('Student', submission.student)
+
+    attached_files = frappe.get_all('File', filters={'attached_to_doctype': 'LMS Assignment', 'attached_to_name': assignment.name}, fields=['file_name', 'file_url'])
+
+    student_files = frappe.get_all('File', filters={'attached_to_doctype': 'Assignment Submission', 'attached_to_name': submission.name}, fields=['file_name', 'file_url'])
+
+    assessment_criteria = []
+    if hasattr(assignment, 'assessment_criteria'):
+        assessment_criteria = assignment.get('assessment_criteria')
+
+    return {
+        'question_text': assignment.question,
+        'assignment_files': attached_files,
+        'student_answer': submission.answer,
+        'student_files': student_files,
+        'comment': submission.comment,
+        'assessment_criteria': assessment_criteria
+    }
