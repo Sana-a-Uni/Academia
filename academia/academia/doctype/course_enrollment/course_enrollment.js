f2cc6c89dfc1367a1f2b129a6a72a739070d26d3
Mohammed Altheeb <midoaltheeb@gmail.com>
2024-07-27 02:55:49 +0300

    feat(core): Add a filters for course enrollment doctype

--- academia/academia/doctype/course_enrollment/course_enrollment.js
+++ academia/academia/doctype/course_enrollment/course_enrollment.js
@@ -1,8 +1,27 @@
 // Copyright (c) 2024, SanU and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Course Enrollment", {
-// 	refresh(frm) {
+frappe.ui.form.on("Course Enrollment", {
+	faculty: function(frm) {
+        var faculty = frm.doc.faculty;
+        frm.set_query("faculty_department", function() {
+            return {
+                "filters": {
+                    "faculty": faculty
+                }
+            };
+        });
+        
+        frm.set_query("academic_program", function(){
+            return {
+                "filters":{
+                    "faculty": faculty
+                }
+            };
+        });
 
-// 	},
-// });
+        frm.set_value('faculty_department', '');
+        frm.set_value('academic_program', '');
+        frm.set_value('student_batch', '');
+    },
+});
