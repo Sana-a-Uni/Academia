833073e2610ad044a2ca98ab70e4f79c878557a4
Ghamdan Mohammed Al-Yamani <140916121+GhamdanAlYamani@users.noreply.github.com>
2024-07-29 02:00:23 +0300

    feat: Created decision module
    and created doctypes.

---
+++ academia/decisions/doctype/main_decision_category/test_main_decision_category.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Ghamdan Alyamani and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMainDecisionCategory(FrappeTestCase):
+	pass
