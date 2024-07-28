62ab65224dc42d78d2f887fc65a3da45d82d7960
Ghamdan Mohammed Al-Yamani <140916121+GhamdanAlYamani@users.noreply.github.com>
2024-07-29 01:56:32 +0300

    Delete academia/decisions directory

--- academia/decisions/doctype/main_decision_category/main_decision_category.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Ghamdan Alyamani and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Main Decision Category", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- academia/decisions/doctype/main_decision_category/main_decision_category.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Ghamdan Alyamani and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class MainDecisionCategory(Document):
-	pass
