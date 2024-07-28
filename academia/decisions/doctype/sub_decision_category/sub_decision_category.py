3a439319cb73fdfd33b2053615f693ff0afb3c4f
Ghamdan Mohammed Al-Yamani <140916121+GhamdanAlYamani@users.noreply.github.com>
2024-07-29 01:23:40 +0300

    Add files via upload
    feat: Create decisions module and its doctypes.

---
+++ academia/decisions/doctype/sub_decision_category/sub_decision_category.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Ghamdan Alyamani and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class SubDecisionCategory(Document):
+	pass
