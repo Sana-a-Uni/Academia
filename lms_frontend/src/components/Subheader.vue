4c2020d77986ad893b7f1a73ca8adae0165ab1e1
khawlaalareeqi <khawlaalareeqi@gmail.com>
2024-07-30 16:30:40 +0300

    Style(LMS): Subheader Component

---
+++ lms_frontend/src/components/Subheader.vue
@@ -0,0 +1,33 @@
+<template>
+<p>2024-2023 - First Semester - Algorithm Course</p>
+</template>
+
+<style scoped>
+p{
+    flex: 1;
+	display: flex;
+    justify-content: flex-start;
+margin-top:55px;
+background-color:white;
+padding:15px;
+padding-left:20px;
+border-bottom: 1px solid black;
+border-color:grey;
+font-size:20px;
+}
+
+@media (max-width: 768px) {
+	p{
+        margin:0px;
+        margin-top:40px;
+    }
+}
+@media (min-width: 768px) {
+	p{
+        margin:0px;
+        margin-top:52px;
+        margin-left:325px;
+        margin-right:10px;
+    }
+}
+</style>
