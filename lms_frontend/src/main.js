4a316c1220c2c985d93604b836b46f8e858179ad
xair <aldubai.atrab@gmail.com>
2024-08-02 19:02:57 +0300

    chore(LMS): Add package

--- lms_frontend/src/main.js
+++ lms_frontend/src/main.js
@@ -4,15 +4,20 @@ import router from "./router";
 import { createPinia } from "pinia";
 import { QuillEditor } from "@vueup/vue-quill";
 import VTooltip from "v-tooltip";
+import { useCourseStore } from "@/stores/teacherStore/courseStore"; 
 
 import "@vueup/vue-quill/dist/vue-quill.snow.css";
 
 const pinia = createPinia();
-
 const app = createApp(App);
+
 app.use(pinia);
 app.use(VTooltip);
-
 app.use(router);
+
 app.component("QuillEditor", QuillEditor);
+
 app.mount("#app");
+
+const courseStore = useCourseStore(pinia);
+courseStore.loadSelectedCourse();
