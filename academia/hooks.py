4d60f5cdad88285ff999a46962d1f80d1bac0cc1
hamedshehab <Hamedshehab2001@gmail.com>
2024-12-10 17:59:00 +0300

    feat(transaction): added client script to not let employee see other employees details among other features

--- academia/hooks.py
+++ academia/hooks.py
@@ -99,7 +99,7 @@ app_include_js = "academia.bundle.js"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-# doctype_js = {"doctype" : "public/js/doctype.js"}
+doctype_js = {"Employee": "public/js/employee.js"}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
