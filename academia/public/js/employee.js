330b003a461e0551578df79b786c01c3c7efe7d3
Anas Al-Dharei <anas202255@gmail.com>
2024-12-11 19:53:16 +0300

    Update employee.js

--- academia/public/js/employee.js
+++ academia/public/js/employee.js
@@ -3,8 +3,10 @@
 frappe.ui.form.on("Employee", {
 	refresh(frm) {
 		// frm.toggle_display(['employment_details', 'contact_details'], frappe.session.user !== frm.doc.user_id);
-		if (frappe.session.user !== frm.doc.user_id) {
-			frappe.set_route("List", "Employee");
+		if(frappe.session.user !== "Administrator" && !frappe.user.has_role("HR Manager") && !frappe.user.has_role("HR Supervisor")) {
+			if (frappe.session.user !== frm.doc.user_id) {
+				frappe.set_route("List", "Employee");
+			}
 		}
 	},
 });
