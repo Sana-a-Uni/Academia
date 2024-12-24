7c34bf7a289c6736e2fd74ef7c5666a396b114cd
hamedshehab <Hamedshehab2001@gmail.com>
2024-12-24 12:59:33 +0300

    feat(transactions): get recipients button now works accordingly

--- academia/transactions/doctype/outbox_memo/outbox_memo.js
+++ academia/transactions/doctype/outbox_memo/outbox_memo.js
@@ -1,8 +1,152 @@
 // Copyright (c) 2024, SanU and contributors
 // For license information, please see license.txt
+let mustInclude = [];
 
-// frappe.ui.form.on("Outbox Memo", {
-// 	refresh(frm) {
+frappe.ui.form.on("Outbox Memo", {
+	refresh(frm) {},
 
-// 	},
-// });
+	onload: function (frm) {
+		if (!frm.doc.start_from) {
+			frappe.call({
+				method: "frappe.client.get_value",
+				args: {
+					doctype: "Employee",
+					filters: { user_id: frappe.session.user },
+					fieldname: "name",
+				},
+				callback: function (response) {
+					if (response.message) {
+						frm.set_value("start_from", response.message.name);
+					}
+				},
+			});
+		}
+	},
+
+	start_from: function (frm) {
+		update_must_include(frm);
+	},
+
+	direction: function (frm) {
+		update_must_include(frm);
+	},
+
+	get_recipients: function (frm) {
+		let setters = {
+			employee_name: null,
+			department: null,
+			designation: null,
+		};
+
+		new frappe.ui.form.MultiSelectDialog({
+			doctype: "Employee",
+			target: frm,
+			setters: setters,
+
+			// add_filters_group: 1,
+			// date_field: "transaction_date",
+			get_query() {
+				let filters = {
+					docstatus: ["!=", 2],
+					user_id: ["in", mustInclude],
+				};
+				if (frm.doc.type == "External") {
+					filters.company = this.setters.company || "";
+				}
+				return {
+					filters: filters,
+				};
+			},
+			primary_action_label: __("Get Recipients"),
+
+			action(selections) {
+				// console.log(d.dialog.get_value("company"));
+				// emptying Council members
+				frm.set_value("recipients", []);
+				// Hold employee names
+				frappe.call({
+					method: "frappe.client.get_list",
+					args: {
+						doctype: "Employee",
+						filters: { name: ["in", selections] },
+						fields: [
+							"name",
+							"employee_name",
+							"designation",
+							"department",
+							"company",
+							"user_id",
+						],
+					},
+					callback: (response) => {
+						var selectedEmployees = response.message;
+
+						// frm.set_value('recipients', []);
+
+						selectedEmployees.forEach((employee) => {
+							frm.add_child("recipients", {
+								recipient: employee.name, // Use document name instead of employee_name
+								recipient_name: employee.employee_name,
+								recipient_company: employee.company,
+								recipient_department: employee.department,
+								recipient_designation: employee.designation,
+								recipient_email: employee.user_id,
+							});
+						});
+
+						this.dialog.hide();
+
+						frm.refresh_field("recipients");
+
+						// // Hide the "Add" button for the recipients table if through_route is checked and there's a recipient
+						// frm.get_field("recipients").grid.grid_buttons.find(".grid-add-row").toggle(!frm.doc.through_route || existingRecipients.length === 0);
+					},
+				});
+			},
+		});
+	},
+
+	clear_recipients: function (frm) {
+		frm.clear_table("recipients");
+		frm.refresh_field("recipients");
+	},
+});
+
+function update_must_include(frm) {
+	if (frm.doc.start_from) {
+		frm.clear_table("recipients");
+		frm.refresh_field("recipients");
+
+		if (frm.doc.type === "External") {
+			frappe.call({
+				method: "academia.transactions.doctype.outbox_memo.outbox_memo.get_all_employees_except_start_from_company",
+				args: {
+					start_from_company: frm.doc.start_from_company,
+				},
+				callback: function (response) {
+					mustInclude = response.message;
+				},
+			});
+		} else if (frm.doc.type === "Internal" && frm.doc.direction !== "Downward") {
+			frappe.call({
+				method: "academia.transactions.doctype.outbox_memo.outbox_memo.get_reports_to_hierarchy",
+				args: {
+					employee_name: frm.doc.start_from,
+				},
+				callback: function (response) {
+					mustInclude = response.message;
+				},
+			});
+		} else {
+			frappe.call({
+				method: "academia.transactions.doctype.outbox_memo.outbox_memo.get_reports_to_hierarchy_reverse",
+				args: {
+					employee_name: frm.doc.start_from,
+				},
+				callback: function (response) {
+					mustInclude = response.message;
+				},
+			});
+		}
+	}
+}
