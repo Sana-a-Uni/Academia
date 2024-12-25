23d5c43b760271c856fc999668cd35054711280b
OmarNamshah <samnoh404@gmail.com>
2024-12-25 14:48:34 +0300

    feat(transactions):added get recipients functionality

--- academia/transactions/doctype/request/request.js
+++ academia/transactions/doctype/request/request.js
@@ -1,8 +1,118 @@
 // Copyright (c) 2024, SanU and contributors
 // For license information, please see license.txt
+let mustInclude = [];
+frappe.ui.form.on("Request", {
+	refresh(frm) {
+		update_must_include(frm);
+	},
+	start_from: function (frm) {
+		update_must_include(frm);
+	},
+	get_recipients: function (frm) {
+		let setters = {
+			employee_name: null,
+			department: null,
+			designation: null,
+		};
+		if (frm.doc.type == "External") {
+			setters.company = null;
+			frappe.call({
+				method: "academia.transaction_management.doctype.transaction.transaction.get_all_employees_except_start_with_company",
+				args: {
+					start_with_company: frm.doc.start_with_company,
+				},
+				callback: function (response) {
+					mustInclude = response.message;
+				},
+			});
+		} else if (frm.doc.type == "Internal") {
+		}
+		new frappe.ui.form.MultiSelectDialog({
+			doctype: "Employee",
+			target: frm,
+			setters: setters,
 
-// frappe.ui.form.on("Request", {
-// 	refresh(frm) {
+			// add_filters_group: 1,
+			// date_field: "transaction_date",
+			get_query() {
+				let filters = {
+					docstatus: ["!=", 2],
+					user_id: ["in", mustInclude],
+				};
+				if (frm.doc.type == "External") {
+					filters.company = this.setters.company || "";
+				}
+				return {
+					filters: filters,
+				};
+			},
+			primary_action_label: __("Get Recipients"),
 
-// 	},
-// });
+			action(selections) {
+				if (selections.length > 1) {
+					frappe.msgprint("You Can Only Select One Recipient");
+					return;
+				}
+				// console.log(d.dialog.get_value("company"));
+				// emptying Council members
+				frm.set_value("recipients", []);
+				// Hold employee names
+				frappe.call({
+					method: "frappe.client.get_list",
+					args: {
+						doctype: "Employee",
+						filters: { name: ["in", selections] },
+						fields: [
+							"name",
+							"employee_name",
+							"designation",
+							"department",
+							"company",
+							"user_id",
+						],
+					},
+					callback: (response) => {
+						var selectedEmployees = response.message;
+
+						// frm.set_value('recipients', []);
+
+						selectedEmployees.forEach((employee) => {
+							frm.add_child("recipients", {
+								recipient: employee.name, // Use document name instead of employee_name
+								recipient_name: employee.employee_name,
+								recipient_company: employee.company,
+								recipient_department: employee.department,
+								recipient_designation: employee.designation,
+								recipient_email: employee.user_id,
+							});
+						});
+
+						this.dialog.hide();
+
+						frm.refresh_field("recipients");
+
+						// // Hide the "Add" button for the recipients table if through_route is checked and there's a recipient
+						// frm.get_field("recipients").grid.grid_buttons.find(".grid-add-row").toggle(!frm.doc.through_route || existingRecipients.length === 0);
+					},
+				});
+			},
+		});
+	},
+});
+
+function update_must_include(frm) {
+	if (frm.doc.start_from) {
+		frm.clear_table("recipients");
+		frm.refresh_field("recipients");
+
+		frappe.call({
+			method: "academia.transactions.doctype.request.request.get_reports_to_hierarchy",
+			args: {
+				employee_name: frm.doc.start_from,
+			},
+			callback: function (response) {
+				mustInclude = response.message;
+			},
+		});
+	}
+}
