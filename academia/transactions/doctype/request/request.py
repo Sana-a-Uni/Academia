3bb1993952ff75c013c11ac2fc0cb0a2a1605f57
Mohammed Altheeb <altheebinfo@gmail.com>
2024-12-28 18:23:32 +0300

    feat(transactions): Add actions to request doctype

--- academia/transactions/doctype/request/request.py
+++ academia/transactions/doctype/request/request.py
@@ -12,17 +12,17 @@ class Request(Document):
 	from typing import TYPE_CHECKING
 
 	if TYPE_CHECKING:
-		from frappe.types import DF
-
 		from academia.transactions.doctype.transaction_attachments_new.transaction_attachments_new import (
 			TransactionAttachmentsNew,
 		)
 		from academia.transactions.doctype.transaction_recipients_new.transaction_recipients_new import (
 			TransactionRecipientsNew,
 		)
+		from frappe.types import DF
 
 		amended_from: DF.Link | None
 		attachments: DF.Table[TransactionAttachmentsNew]
+		current_action_maker: DF.Data | None
 		full_electronic: DF.Check
 		recipients: DF.Table[TransactionRecipientsNew]
 		start_from: DF.Link
@@ -49,3 +49,88 @@ def get_reports_to_hierarchy(employee_name):
 		reports_to = employee.reports_to
 
 	return reports_emails
+
+
+@frappe.whitelist()
+def create_new_request_action(user_id, request, type, details):
+	"""
+	Create a new document in Transaction Action and pass the relevant data from Transaction.
+	This function will be called when a button is pressed in Transaction.
+	"""
+	request_doc = frappe.get_doc("Request", request)
+
+	action_maker = request_doc.current_action_maker
+	if action_maker:
+		new_doc = frappe.new_doc("Request Action")
+		new_doc.request = request
+		new_doc.type = type
+		new_doc.from_company = action_maker.recipient_company
+		new_doc.from_department = action_maker.recipient_department
+		new_doc.from_designation = action_maker.recipient_designation
+		new_doc.details = details
+		new_doc.action_date = frappe.utils.today()
+		new_doc.created_by = action_maker.recipient_email
+		new_doc.save(ignore_permissions=True)
+		new_doc.submit()
+
+		action_name = new_doc.name
+
+		# check_result = check_all_recipients_action(inbox_memo, user_id)
+
+		# if check_result:
+		# 	inbox_memo_doc = frappe.get_doc("Transaction", transaction_name)
+
+		# 	next_step = inbox_memo_doc.step + 1
+		# 	next_step_recipients = frappe.get_all(
+		# 		"Transaction Recipients",
+		# 		filters={"parent": inbox_memo_doc.name, "step": ("=", next_step)},
+		# 		fields=["recipient_email", "step"],
+		# 	)
+		# 	if len(next_step_recipients) > 0:
+		# 		for recipient in next_step_recipients:
+		# 			frappe.share.add(
+		# 				doctype="Transaction",
+		# 				name=inbox_memo_doc.name,
+		# 				user=recipient.recipient_email,
+		# 				read=1,
+		# 				write=1,
+		# 				share=1,
+		# 				submit=1,
+		# 			)
+		# 		inbox_memo_doc.step = next_step
+		# 	else:
+		if type == "Approved":
+			request_doc.status = "Completed"
+		elif type == "Rejected":
+			request_doc.status = "Rejected"
+
+		request_doc.complete_time = frappe.utils.now()
+		request_doc.current_action_maker = ""
+
+		request_doc.save(ignore_permissions=True)
+
+		permissions = {"read": 1, "write": 0, "share": 0, "submit": 0}
+		permissions_str = json.dumps(permissions)
+		update_share_permissions(request, user_id, permissions_str)
+
+		return {"message": "Action Success", "action_name": action_name}
+	else:
+		return {"message": "No employee found for the given user ID."}
+
+
+@frappe.whitelist()
+def update_share_permissions(docname, user, permissions):
+	share = frappe.get_all(
+		"DocShare",
+		filters={"share_doctype": "Request", "share_name": docname, "user": user},
+	)
+	permissions_dict = json.loads(permissions)
+	if share:
+		# Share entry exists, update the permissions
+		share = frappe.get_doc("DocShare", share[0].name)
+		share.update(permissions_dict)
+		share.save(ignore_permissions=True)
+		frappe.db.commit()
+		return share
+	else:
+		return "text"
