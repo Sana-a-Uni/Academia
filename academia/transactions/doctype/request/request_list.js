2efbc6fc7bccc5a2364cff978731262e855386a3
Anas Al-Dharei <anas202255@gmail.com>
2025-01-18 15:40:59 +0300

    feat(transactions): add filter to request doctype to view only records that related to user

--- academia/transactions/doctype/request/request_list.js
+++ academia/transactions/doctype/request/request_list.js
@@ -1,7 +1,26 @@
 frappe.listview_settings['Request'] = {
-    onload: function() {
+    onload: function(listview) {
         $('.btn-primary').filter(function () {
             return $(this).text().trim() === 'Add Request';
         }).hide();
+
+        if(!frappe.user_roles.includes('Transactions Manager')) {
+            frappe.call({
+                method: 'academia.transactions.doctype.request.request.get_shared_requests',
+                args: {
+                    user: frappe.session.user
+                },
+                callback: function(response) {
+                    if (response.message) {
+                        const allowed_memos = response.message;
+                        listview.filter_area.add([
+                            ['Request', 'name', 'in', allowed_memos]
+                        ]);
+    
+                        $('.filter-selector').hide();
+                    }
+                }
+            });
+        }
     }
 };
