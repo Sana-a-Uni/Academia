5b973c5103f6ca49cfa8ab336a0554f8a004399e
hamedshehab <Hamedshehab2001@gmail.com>
2025-01-18 17:20:10 +0300

    feat(transactions): added filtering for outbox and std

--- academia/transactions/doctype/outbox_memo/outbox_memo_list.js
+++ academia/transactions/doctype/outbox_memo/outbox_memo_list.js
@@ -1,7 +1,26 @@
 frappe.listview_settings['Outbox Memo'] = {
-    onload: function() {
+    onload: function(listview) {
         $('.btn-primary').filter(function () {
             return $(this).text().trim() === 'Add Outbox Memo';
         }).hide();
+
+        if(!frappe.user_roles.includes('Transactions Manager')) {
+            frappe.call({
+                method: 'academia.transactions.doctype.outbox_memo.outbox_memo.get_shared_outbox_memos',
+                args: {
+                    user: frappe.session.user
+                },
+                callback: function(response) {
+                    if (response.message) {
+                        const allowed_memos = response.message;
+                        listview.filter_area.add([
+                            ['Outbox Memo', 'name', 'in', allowed_memos]
+                        ]);
+    
+                        $('.filter-selector').hide();
+                    }
+                }
+            });
+        }
     }
 };
