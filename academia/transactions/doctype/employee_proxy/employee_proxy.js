d51687cf5e4debee10ae73d88101817f7d87c089
hamedshehab <Hamedshehab2001@gmail.com>
2025-01-29 17:23:56 +0300

    feat(transactions): added secretary functionality to transaction new and made employee proxy unique based on company and department and designation

--- academia/transactions/doctype/employee_proxy/employee_proxy.js
+++ academia/transactions/doctype/employee_proxy/employee_proxy.js
@@ -1,8 +1,33 @@
 // Copyright (c) 2025, SanU and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Employee Proxy", {
-// 	refresh(frm) {
+frappe.ui.form.on("Employee Proxy", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+    before_save(frm) {
+        frappe.call({
+            method: "frappe.client.get_list",
+            args: {
+                doctype: "Employee Proxy",
+                filters: {
+                    company: frm.doc.company,
+                    department: frm.doc.department,
+                    designation: frm.doc.designation,
+                    name: ["!=", frm.doc.name] // Exclude the current record
+                },
+                fields: ["name"]
+            },
+            callback: function(response) {
+                if (response.message.length > 0) {
+                    frappe.msgprint({
+                        title: __('Error'),
+                        indicator: 'red',
+                        message: __("A record with the same Company, Department, and Designation already exists.")
+                    });
+                    frappe.validated = false;
+                }
+            }
+        });
+    }
+});
